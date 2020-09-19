87c67fe9d878e8d10896a8fc43b0f90755f5a888
Soul-Aspiro <70144210+Soul-Aspiro@users.noreply.github.com>
2020-09-19 04:01:13 -0700

    changed

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Web Design and Development'
+copyright = '2020, Vijay'
+author = 'Vijay'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
